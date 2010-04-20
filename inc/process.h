5c991a54b3057a6d313f7b30c04423c92acace33
nicospuri <nicospuri@c06c0ad2-8b28-93df-a945-e75a71ef55d8>
2010-04-20 11:43:48 +0000

    Se agregan funciones correspondientes a procesos, como getpid etc

--- inc/process.h
+++ inc/process.h
@@ -4,6 +4,8 @@
 #include "syscall.h"
 #include "int80.h"
 #include "types.h"
+#include "sysProcess.h"
+#include "string.h"
 
 #define FOREGROUND		0
 #define BACKGROUND		1
@@ -15,5 +17,15 @@ typedef int tty_t;
 typedef void (pfunc_t)(int);
 
 void * memmap();
+
+pid_t getpid();
+
+pid_t getppid();
+
+tty_y getCurrentTTY();
+
+proccess_t * getProccessByPid( pid_t pid );
+
+proccess_t * getProccessByName( char * name );
 
 #endif
