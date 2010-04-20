31b41bf30c11204e6fe5b07880132ab344e78611
nicospuri <nicospuri@c06c0ad2-8b28-93df-a945-e75a71ef55d8>
2010-04-20 01:39:26 +0000

    Se comienza con la implementacion del scheduler

--- src/sysProcess.c
+++ src/sysProcess.c
@@ -7,3 +7,12 @@ process_t *runningProcess = NULL;
 process_t *initProcess = NULL;
 
 process_t *nextProcess = NULL;
+
+pid_t currentPID = 0;
+
+static pid_t nextPID = 1;
+
+sysTTY ttyTable;
+
+int qtyProccessTable = 0;
+
