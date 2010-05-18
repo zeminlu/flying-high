a54b95f123bd75b122fab0c97dbcb94ee8b2bc41
zeminlu <zeminlu@c06c0ad2-8b28-93df-a945-e75a71ef55d8>
2010-05-18 02:29:04 +0000

    Listooooo!!!!!! ABRIENDO EL SHAMPOO!!!

--- trunk/inc/sysMalloc.h
+++ trunk/inc/sysMalloc.h
@@ -16,10 +16,10 @@
 /**
  * \fn void sysFree(void *ap, memArea_t * (*getMemoryArea)())
  *
- * 		\brief leaves avaiable the segment to be used in futures calls to sysMalloc. The memory
+ * 		\brief Leaves avaiable the segment to be used in futures calls to sysMalloc. The memory
  *  			segment is taken from the function pased by arguments.
  *
- * 		\param ap the logical memory segment to be freed.
+ * 		\param ap The logical memory segment to be freed.
  * 		\param getMemoryArea Function that returns the memory frame that sysMalloc wold be used in the future.
  * 		
  * 		\sa sysRealloc() sysMalloc()
@@ -30,14 +30,14 @@ void sysFree(void *ap, memArea_t * (*getMemoryArea)());
 /**
  * \fn void *sysMalloc(size_t nbytes, memArea_t * (*getMemoryArea)())
  *
- * 		\brief Function that gives a continues segment of memory. This memory becames from the
+ * 		\brief Function that gives a continuous segment of memory. This memory comes from the
  * 				function getMemoryArea. It could return null if there is not enougth free space
  *				on that frame.
  *
- * 		\param nbytes the size of continues segment memory.
+ * 		\param nbytes the size of continuous segment memory.
  * 		\param getMemoryArea Functions that gives the frame to use be used by sysMalloc.
  * 		
- * 		\return The continues segment memory.
+ * 		\return The continuous segment of memory.
  *
  * 		\sa sysFree() sysRealloc()
  *
@@ -47,14 +47,14 @@ void *sysMalloc(size_t nbytes, memArea_t * (*getMemoryArea)());
 /**
  * \fn void *sysRealloc(void *ptr, size_t size, memArea_t * (*getMemoryArea)())
  *
- * 		\brief This function change the original size of the logical memory to a new one if
- *  				it's necesary.Returns null if it's not enought free space.
+ * 		\brief This function changes the original size of the logical memory to a new one if
+ *  				it's necesary.Returns null if theres not enough free space.
  *
- * 		\param ptr old logical segment memory, that could be moved to a new one.
- * 		\param size The new size of the continues segment memory.
- * 		\param getMemoryArea Functions that gives the frame to use be used by sysMalloc.
+ * 		\param ptr Old logical memory segment, that could be moved to a new one.
+ * 		\param size The new size of the continuous memory segment.
+ * 		\param getMemoryArea Function that gives the frame to use be used by sysMalloc.
  * 		
- * 		\return The new continues segment memory.
+ * 		\return The new continuous memory segment.
  *
  * 		\sa sysFree() sysMalloc()
  *
